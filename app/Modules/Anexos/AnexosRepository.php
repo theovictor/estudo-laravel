a9bc975f84c6946b503d03f89a6a96403e987769
Theo Victor <teo.pvh@gmail.com>
2023-08-04 12:28:21 -0400

    att repo anexos

--- app/Modules/Anexos/AnexosRepository.php
+++ app/Modules/Anexos/AnexosRepository.php
@@ -1,15 +1,18 @@
 <?php namespace App\Modules\Anexos;
 
+use Illuminate\Support\Facades\Storage;
+
 class AnexosRepository implements AnexosInterface {
   public function __construct(private AnexosModel $model) {
     $this->model = $model;
   }
 
   public function upload($file) {
-
+    return Storage::put('anexos', $file);
   }
 
-  public function download($file) {
-
+  public function download($filename) {
+    $path = 'caminho do arquivo no storage min io';
+    return Storage::download($path, $filename);
   }
 }
